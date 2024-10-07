d100e24920d9a9ac5700ba667da577a6238872d8
ragesh <ragesh66kr@gmail.com>
2024-10-07 20:23:29 +0530

    add tqdm for processing

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import argparse
 from data_extraction import pdf_extract
-import getpass
 import os
+from tqdm import tqdm
 
 def call():
-    path_to_pdf = getpass.getpass("Path to data : ")
+    path_to_pdf = input("Path to data : ")
 
     # check if the path is valid pdf
     if not os.path.exists(path_to_pdf) or not os.path.isfile(path_to_pdf) or not path_to_pdf.endswith('.pdf'): 
@@ -19,13 +19,16 @@ def call():
     
     json_data = pdf_extract.extract(path_to_pdf)
  
+    print("data chunking completed")
     if category == 'vision':
         from models import v_llm
-        data = [v_llm.main(doc, requested_schema) for doc in json_data]
+        data = [v_llm.main(doc, requested_schema) for doc in tqdm(json_data, desc="Processing chunks")]
     else:
         from models import llm
-        data = [llm.main(doc, requested_schema) for doc in json_data]
-
+        data = [llm.main(doc, requested_schema) for doc in tqdm(json_data, desc="Processing chunks")]
+    print("Done!")
+    return data
 
 if __name__ == "__main__":
     call()
+
